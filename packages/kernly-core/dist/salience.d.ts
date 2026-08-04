98c9ccc3cff5a0fdc8a97619fe55f954ebe0c873
Bryan Kwandou <nayrbryangaming01@gmail.com>
2026-08-04 11:05:53 +0800

    chore: stop tracking build output and installed packages

--- packages/kernly-core/dist/salience.d.ts
+++
@@ -1,2 +0,0 @@
-import type { Block } from "./types.js";
-export declare function score(blocks: Block[], query?: string): Block[];
--- packages/kernly-core/dist/segment.d.ts
+++
@@ -1,3 +0,0 @@
-import type { Block } from "./types.js";
-export declare function normalize(input: string): string;
-export declare function segment(input: string, pinPatterns?: RegExp[]): Block[];
